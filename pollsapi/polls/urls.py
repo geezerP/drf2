e05cf9c10843deb14758b4a1072a1f823747c928
geezerP <geezerP@yahoo.com>
2020-10-14 20:47:25 +0300

    Choices and Polls

--- pollsapi/polls/urls.py
+++ pollsapi/polls/urls.py
@@ -1,7 +1,9 @@
 # from django.urls import path
 from django.conf.urls import url
-from .views import PollDetail, PollList
+from .views import ChoiceList, CreateVote, PollDetail, PollList
 urlpatterns = [
     url('polls/', PollList.as_view(), name='polls_list'),
-    url('polls/<int:pk>/', PollDetail.as_view(), name='polls_detail')
+    url('polls/<int:pk>/', PollDetail.as_view(), name='polls_detail'),
+    url('choices/', ChoiceList.as_view(), name="choice_list"),
+    url('vote/', CreateVote.as_view(), name="create_vote"),
 ]
--- pollsapi/pollsapi/urls.py
+++ pollsapi/pollsapi/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')), 
     url(r'^admin/', admin.site.urls),
     url('', include('polls.urls')),
 ]
